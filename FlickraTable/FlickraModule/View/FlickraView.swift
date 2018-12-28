4dc8bcbf787e1efc472bcf07c078c8e923d24c76
ArturRUZ <artur.ruzhnikov@icloud.com>
2018-12-28 11:32:41 +0300

    PhotosResponse is working

--- FlickraTable/FlickraModule/View/FlickraView.swift
+++ FlickraTable/FlickraModule/View/FlickraView.swift
@@ -39,7 +39,7 @@ extension FlickraViewController : FlickraViewtViewInput {
 
 extension FlickraViewController {
     private func demoNetworking() {
-        let url = URL(string: "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22")!
+        let url = URL(string: "https://www.flickr.com/services/rest?method=flickr.interestingness.getList&api_key=3988023e15f45c8d4ef5590261b1dc53&per_page=40&page=1&format=json&nojsoncallback=1&extras=url_l&date=2018-09-23")!
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else { return }
             DispatchQueue.main.async {
@@ -53,8 +53,9 @@ extension FlickraViewController {
         print("длина байтов -= \(data.count)")
         
         do {
-            let weatherResponse = try JSONDecoder().decode(WeatherResponse.self, from: data)
-            print(weatherResponse)
+            let photosResponse = try JSONDecoder().decode(PhotosResponse.self, from: data)
+            print (photosResponse.photos.photo[1].url_l)
+            //print(photosResponse)
         } catch {
             print("error = \(error.localizedDescription)")
         }
